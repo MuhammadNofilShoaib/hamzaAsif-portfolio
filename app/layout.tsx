ac3d4d1cc377f7960533af1a868464fe42cd17f5
Nofil Shoaib <nofilshoaib@gmail.com>
2025-04-24 18:38:50 +0500

    Meta Tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,9 +18,32 @@ export const metadata: Metadata = {
   title: "Hamza Asif | Video Editor",
   description: "Helping creators boost watch time with high retention edits",
   icons: {
-    icon: "/webIcon.jpg",
+    icon: "/hamza.png",
+  },
+  openGraph: {
+    title: "Hamza Asif | Video Editor",
+    description: "Helping creators boost watch time with high retention edits",
+    url: "https://hamzaedits.vercel.app/",
+    siteName: "Hamza Asif Portfolio",
+    images: [
+      {
+        url: "https://hamzaedits.vercel.app/hamza.png", // your preferred image URL
+        width: 1200,
+        height: 630,
+        alt: "Hamza Asif - Video Editor",
+      },
+    ],
+    locale: "en_US",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Hamza Asif | Video Editor",
+    description: "Helping creators boost watch time with high retention edits",
+    images: ["https://hamzaedits.vercel.app/hamza.png"],
   },
 };
+
 
 export default function RootLayout({
   children,
